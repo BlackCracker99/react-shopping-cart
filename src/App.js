d83d610a5977db93c54ef45b0683c5e6fa848ef6
blackCracker <fogxcfg@gmail.com>
2021-07-23 11:26:51 +0530

    create redux filters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import data from "./data.json";
-import Produtcs from "./components/Produtcs";
+import Products from "./components/Produtcs";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
 import store from "./store";
 import { Provider } from "react-redux"; 
+
+
 class App extends React.Component {
   constructor() {
     super();
@@ -12,8 +14,6 @@ class App extends React.Component {
       products: data.products,
       //code 1 : this.line -> after we refresh the page but cart items removed or refreshed
       cartItems : localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
-      size: "",
-      sort: "",
     };
   }
   //create order
@@ -49,48 +49,6 @@ class App extends React.Component {
      //code 1 : this.line -> after we refresh the page but cart items removed or refreshed
     localStorage.setItem("cartItems" , JSON.stringify(cartItems));
   };
-  //sort products by price
-  sortProducts = (event) => {
-    // impl
-    const sort = event.target.value;
-    console.log(event.target.value);
-    this.setState((state) => ({
-      sort: sort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === "lowest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : sort === "highest"
-            ? a.price < b.price
-              ? 1
-              : -1
-            : a._id < b._id
-            ? 1
-            : -1
-        ),
-    }));
-  };
-  //filter products 
-  filterProducts = (event) => 
-  {
-    //imp
-    console.log(event.target.value);
-    if(event.target.value === "")
-    {
-      this.setState({size:event.target.value , products:data.products});
-    }
-    else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }
-  };
 
   render() {
     return (
@@ -100,30 +58,20 @@ class App extends React.Component {
           <a href="/">React Shopping Cart</a>
         </header>
         <main>
-          <div className="content">
-            <div className="main">
-              <Filter count={this.state.products.length}
-                      size = {this.state.size}
-                      sort = {this.state.sort}
-                      filterProducts = {this.filterProducts}
-                      sortProducts = {this.sortProducts}
-              ></Filter>
-              <Produtcs 
-              products={this.state.products}
-              addToCart={this.addToCart}
-              ></Produtcs>
-              Products
+            <div className="content">
+              <div className="main">
+                <Filter></Filter>
+                <Products addToCart={this.addToCart}></Products>
+              </div>
+              <div className="sidebar">
+                <Cart
+                  cartItems={this.state.cartItems}
+                  removeFromCart={this.removeFromCart}
+                  createOrder={this.createOrder}
+                />
+              </div>
             </div>
-
-            <div className="sidebar">
-              <Cart
-                cartItems={this.state.cartItems}
-                removeFromCart={this.removeFromCart}
-                createOrder={this.createOrder}
-              /></div>
-
-          </div>
-        </main>
+          </main>
         <footer>All right is reserved.</footer>
       </div>
       </Provider>
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,5 +1,8 @@
 import { FETCH_PRODUCTS } from "../types";
+import { FILTER_PRODUCTS_BY_SIZE } from "../types";
+import { ORDER_PRODUCTS_BY_PRICE } from "../types";
 
+//fetch redux products  
 export const fetchProducts = () => async (dispatch) => {
   const res = await fetch("/api/products");
   const data = await res.json();
@@ -7,5 +10,47 @@ export const fetchProducts = () => async (dispatch) => {
   dispatch({
     type: FETCH_PRODUCTS,
     payload: data,
+  });
+};
+
+//filter redux products
+export const filterProducts = (products, size) => (dispatch) => {
+  dispatch({
+    type: FILTER_PRODUCTS_BY_SIZE,
+    payload: {
+      size: size,
+      items:
+        size === ""
+          ? products         //return only selected size products by size 
+          : products.filter((x) => x.availableSizes.indexOf(size) >= 0),
+    },
+  });
+};
+
+//sort redux products
+export const sortProducts = (filteredProducts, sort) => (dispatch) => {
+  const sortedProducts = filteredProducts.slice();
+  if (sort === "latest") {
+    //sort by highest
+    sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+  } else {
+    //sort by lowest
+    sortedProducts.sort((a, b) =>
+      sort === "lowest"
+        ? a.price > b.price
+          ? 1
+          : -1
+        : a.price > b.price
+        ? -1
+        : 1
+    );
+  }
+  console.log(sortedProducts);
+  dispatch({
+    type: ORDER_PRODUCTS_BY_PRICE,
+    payload: {
+      sort: sort,
+      items: sortedProducts,
+    },
   });
 };
